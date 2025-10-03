9b7c814f5be7fc72421398bc9d55abaf9f8a4721
root <root@DESKTOP-QTKDFSB.localdomain>
2025-10-03 18:10:51 -0400

    forced package list to be updated

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]; then
   echo "This script must be run as root." >&2
   exit 1
 fi
-
+apt update
 apt install alire=1.2.1-2build1 -y
 apt install build-essential=12.12ubuntu1 -y
 apt install libc6-dev=2.41-6ubuntu1.2 -y
--- helios/src/helios.adb
+++ helios/src/helios.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO;
 
 procedure Helios is
+
 begin
     Ada.Text_IO.Put("Hello World");
 end Helios;
