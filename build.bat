0e982a3337c6ab9d2403a6b68df591cdca2e50f3
jwyffels2 <justinwyffels@gmail.com>
2025-11-08 19:16:49 -0500

    added parameter to script to optionally create tcl as it takes forever

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
 
+@echo off
 pushd "%~dp0"
-call .\build_neorv32.bat
+
+rem Default: skip running create_project.tcl
+
+rem If first arg is "create", enable it
+if /I "%~1"=="--create" (
+    call .\build_neorv32.bat --create
+) else (
+    call .\build_neorv32.bat
+)
+
+
 
 podman build -t helios-build -f ./build.dockerfile .
 
--- build_neorv32.bat
+++ build_neorv32.bat
@@ -2,9 +2,24 @@
 setlocal
 pushd "%~dp0"
 
+rem Default: skip running create_project.tcl
+set DO_CREATE=0
+
+rem If first arg is "create", enable it
+if /I "%~1"=="--create" (
+    set DO_CREATE=1
+)
+
 pushd ".\third_party\helios-neorv32-setups\vivado\basys3-a7-test-setup"
 
-call vivado -mode batch -source "create_project.tcl"
+if "%DO_CREATE%"=="1" (
+    echo Running create_project.tcl ...
+    call vivado -mode batch -source "create_project.tcl"
+) else (
+    echo "Skipping create_project.tcl (default)."
+)
+
+echo "Running program_bitstream.tcl ..."
 call vivado -mode batch -source "program_bitstream.tcl"
 
 popd
