9460ef4c7a06a342cf771ea008c4f6b4905b343b
Woobler <137943389+jwyffels2@users.noreply.github.com>
2025-11-11 20:49:22 -0500

    Implement basic LED blink
    * updated-create-project.tcl
    
    * updated dependent repos

--- src/helios.adb
+++ src/helios.adb
@@ -1,15 +1,64 @@
 with Ada.Text_IO;
--- with Interrupts; Commented out because we don't need to use interrupts unless we want to handle keyboard/async input
-with Uart0; --Must be imported otherwise cannot print to screen, because UART is the protocol used to connect via terminal
-with Gnat_Exit; --Must be imported/implemented so program knows how to exit
-with neorv32; use neorv32; -- This must be used to be able to have access to special type compatibility etc
-with neorv32.GPIO; -- Used to control LEDs and other GPIO
+with Uart0;
+with Gnat_Exit;
+with neorv32;          use neorv32;
+with neorv32.GPIO;
+with neorv32.SYSINFO;
+with neorv32.GPTMR;
+
 procedure Helios is
-LED_Mask : constant neorv32.UInt32 := 2#0000_0000_0000_0000_0000_0000_0000_1111#;  -- gpio_o(0) LED0 On Basys3
+
+   Clock_Hz : constant UInt32 :=
+     neorv32.SYSINFO.SYSINFO_Periph.CLK;  -- e.g. 100_000_000
+
+   -- GPTMR prescaler = f/128  (PRSC = 4 according to NEORV32 docs)
+   Ticks_Per_Second : constant UInt32 := Clock_Hz / 128;
+
+   LED_Mask : constant neorv32.UInt32 :=
+     2#0000_0000_0000_0000_0000_0000_1000_1111#;
+
+   -- wait for ~1 second using GPTMR in single-shot mode
+   procedure Wait_One_Second is
+   begin
+      -- set threshold
+      neorv32.GPTMR.GPTMR_Periph.THRES := Ticks_Per_Second;
+
+      -- clear any old pending interrupt
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_IRQ_CLR := 1;
+
+      -- configure mode and prescaler
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_MODE := 0;  -- single-shot
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_PRSC := 4;  -- f/128
+
+      -- enable the timer
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_EN := 1;
+
+      -- poll until threshold reached
+      while neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_IRQ_PND = 0 loop
+         null;
+      end loop;
+
+      -- ack/clear pending flag (and optionally stop timer)
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_IRQ_CLR := 1;
+      neorv32.GPTMR.GPTMR_Periph.CTRL.GPTMR_CTRL_EN      := 0;
+   end Wait_One_Second;
+
+   -- "Seconds" is now N * real timer seconds
+   procedure Timer (Seconds : UInt32) is
+   begin
+      for S in 1 .. Seconds loop
+         Wait_One_Second;
+      end loop;
+   end Timer;
+
 begin
-   Ada.Text_IO.Put_Line("Hello");
-   neorv32.GPIO.GPIO_Periph.PORT_OUT := neorv32.GPIO.GPIO_Periph.PORT_OUT or LED_Mask;
+   Ada.Text_IO.Put_Line ("Hello");
+
+    neorv32.GPIO.GPIO_Periph.PORT_OUT := LED_Mask;
+
    loop
-      null;
+      Timer (1);
+      -- clean toggle of only the LED bits
+      neorv32.GPIO.GPIO_Periph.PORT_OUT := neorv32.GPIO.GPIO_Periph.PORT_OUT xor LED_Mask;
    end loop;
 end Helios;
