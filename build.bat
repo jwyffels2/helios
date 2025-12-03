ff254b2b41e28efde4bb69a890911ca4b474af8f
Woobler <137943389+jwyffels2@users.noreply.github.com>
2025-12-02 22:17:21 -0500

    Make test suite (#19)
    * Added new tests binary project
    
    * created common build gpr script
    
    * updated build scripts
    
    * updated build scripts
    
    * updated build scripts
    
    * added git ignore
    
    * nuked files
    
    * nuked files

--- build.bat
+++ build.bat
@@ -1,19 +1,42 @@
-
 @echo off
+setlocal
 pushd "%~dp0"
 
-rem Default: skip running build.tcl
+rem ----------------------------------------------------
+rem Parse flags from command line
+rem ----------------------------------------------------
+set "DO_CREATE=0"
+set "DO_TEST=0"
 
-rem If first arg is "create", enable it
-if /I "%~1"=="--create" (
+for %%A in (%*) do (
+    if /I "%%~A"=="--create" set "DO_CREATE=1"
+    if /I "%%~A"=="--test"   set "DO_TEST=1"
+)
+
+rem ----------------------------------------------------
+rem Optional: create image + run build_neorv32.bat with --create
+rem ----------------------------------------------------
+if %DO_CREATE%==1 (
     podman build --no-cache -t helios-build -f ./build.dockerfile .
     call .\build_neorv32.bat --create
 ) else (
     call .\build_neorv32.bat
 )
 
+rem Always do a normal build as well
 podman build -t helios-build -f ./build.dockerfile .
 
-podman run --rm -v "%CD% :/workspace" -w /workspace helios-build ./build_hal_fw.sh
+rem ----------------------------------------------------
+rem Choose ELF path depending on --test
+rem ----------------------------------------------------
+if %DO_TEST%==1 (
+    set "ELF_PATH=./tests/bin/tests"
+) else (
+    set "ELF_PATH=./bin/helios"
+)
+
+podman run --rm -v "%CD%:/workspace" -w /workspace helios-build ^
+    ./build_neorv32_project.sh "%ELF_PATH%"
 
 popd
+endlocal
