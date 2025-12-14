9882038c56f451d89cbfbf3485c2436db2f78f67
jwyffels2 <justinwyffels@gmail.com>
2025-12-14 16:40:25 -0500

    made demo

--- src/helios.adb
+++ src/helios.adb
@@ -1,13 +1,31 @@
 with VGA;
 with Interfaces; use Interfaces;
 with PWM_API;
+with Uart0;
+with Ada.Text_IO; use Ada.Text_IO;
 with Gnat_Exit;
-procedure helios is
-
+procedure Helios is
 begin
+   loop
+      VGA.Enable;
+      VGA.Set_Background (16#0#, 16#0#, 16#8#); -- blue-ish
 
-        -- Turn VGA on
-        VGA.Enable;
-        VGA.Set_Background (16#0#, 16#0#, 16#8#); -- blue-ish background
+      for I in 1 .. 100 loop
+         Put_Line ("Forcing Delay");
+      end loop;
 
-end helios;
+      VGA.Set_Background (16#8#, 16#0#, 16#0#); -- red-ish
+
+      for I in 1 .. 100 loop
+         Put_Line ("Forcing Delay");
+      end loop;
+
+        VGA.Set_Background (16#0#, 16#8#, 16#0#); -- green-ish
+
+      for I in 1 .. 100 loop
+         Put_Line ("Forcing Delay");
+      end loop;
+
+
+   end loop;
+end Helios;
