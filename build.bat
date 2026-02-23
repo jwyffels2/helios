e9886b5f3df09a515291a534a6ebb4c5b52ede47
jwyffels2 <justinwyffels@gmail.com>
2026-02-23 14:17:37 -0500

    Added ping command

--- build.bat
+++ build.bat
@@ -7,11 +7,14 @@ rem Parse flags from command line
 rem ----------------------------------------------------
 set "DO_CREATE=0"
 set "DO_TEST=0"
+set "DO_PROGRAM=0"
 
 for %%A in (%*) do (
     if /I "%%~A"=="--create" set "DO_CREATE=1"
     if /I "%%~A"=="--test"   set "DO_TEST=1"
+    if /I "%%~A"=="--program"   set "DO_PROGRAM=1"
 )
+
 
 rem ----------------------------------------------------
 rem Optional: create image + run build_neorv32.bat with --create
@@ -19,7 +22,8 @@ rem ----------------------------------------------------
 if %DO_CREATE%==1 (
     podman build --no-cache -t helios-build -f ./build.dockerfile .
     call .\build_neorv32.bat --create
-) else (
+)
+if %DO_PROGRAM%==1 (
     call .\build_neorv32.bat
 )
 
--- src/helios.adb
+++ src/helios.adb
@@ -1,20 +1,29 @@
+with Ada.Text_IO;
 with Uart0;
 with Gnat_Exit;
-with PWM_API;  use PWM_API;
-with GPIO_API; use GPIO_API;
-with neorv32;  use neorv32;
+with PWM_API;     use PWM_API;
+with GPIO_API;    use GPIO_API;
+with neorv32;     use neorv32;
 with neorv32.TWI; use neorv32.TWI;
-with TWI; use TWI;
+with TWI;         use TWI;
 
 procedure Helios is
-   -- Create PWM
-   Pwm0: PWM_T := PWM_API.Create (Channel => 0);
+    -- Create PWM
+    Pwm0 : PWM_T := PWM_API.Create (Channel => 0);
+    pingResponse : Boolean;
 begin
 
-   Pwm0.Configure (Target_Hz => 25_000_000.0, Duty => 0.5);
-   Pwm0.Enable;
+    Pwm0.Configure (Target_Hz => 25_000_000.0, Duty => 0.5);
+    Pwm0.Enable;
 
-   TWI.TWI_Setup (preScaler => 6, clockDiv => 15, allowClockStretching => False);
-   TWI.Scan_TWI;
+    TWI.TWI_Setup
+       (preScaler => 6, clockDiv => 15, allowClockStretching => False);
+    TWI.Scan_TWI;
+    pingResponse := TWI.I2C_Ping (16#3C#);
+    if pingResponse then
+        Ada.Text_IO.Put_Line ("Ping Response Recieved");
+    else
+        Ada.Text_IO.Put_Line ("Something is wrong!");
+    end if;
 
 end Helios;
