7d0800297e7e6dd32233fbaa927bcba4201f59f4
jwyffels2 <justinwyffels@gmail.com>
2026-03-08 14:09:11 -0400

    saving main func

--- src/helios.adb
+++ src/helios.adb
@@ -22,22 +22,22 @@ begin
     Pwm0.Enable;
 
     TWI.TWI_Setup (preScaler => 6, clockDiv => 15, allowClockStretching => False);
-    TWI.Scan_TWI;
-    pingResponse := TWI.I2C_Ping (CAMERA_ADDRESS);
-    if pingResponse then
-        Ada.Text_IO.Put_Line ("Ping Response Recieved");
-    else
-        Ada.Text_IO.Put_Line ("Something is wrong!");
-    end if;
-
-    --  TWI.I2C_Write (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER, Value => TEST_PATTERN_CONFIG);
-    --  if TWI.I2C_Read (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER) = TEST_PATTERN_CONFIG then
-    --      Ada.Text_IO.Put_Line ("Set Register Properly");
+    --  TWI.Scan_TWI;
+    --  pingResponse := TWI.I2C_Ping (CAMERA_ADDRESS);
+    --  if pingResponse then
+    --      Ada.Text_IO.Put_Line ("Ping Response Recieved");
     --  else
-    --      Ada.Text_IO.Put_Line ("Does Not Work Sir");
-
-    --      output := TWI.I2C_Read (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER)'Image;
-
+    --      Ada.Text_IO.Put_Line ("Something is wrong!");
     --  end if;
+
+    TWI.I2C_Write (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER, Value => TEST_PATTERN_CONFIG);
+    if TWI.I2C_Read (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER) = TEST_PATTERN_CONFIG then
+        Ada.Text_IO.Put_Line ("Set Register Properly");
+    else
+        Ada.Text_IO.Put_Line ("Does Not Work Sir");
+
+        --  output := TWI.I2C_Read (Device_Address => CAMERA_ADDRESS, Register => TEST_PATTERN_REGISTER)'Image;
+
+    end if;
 
 end Helios;
