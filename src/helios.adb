8b5c8b937471984ebbeec326425a3111782110bc
Lendragon102 <Lendragon102@aol.com>
2026-03-10 18:32:15 -0400

    Fix Helios PWM API usage and initialize UART0

--- src/helios.adb
+++ src/helios.adb
@@ -1,15 +1,11 @@
-with Uart0;
 with Gnat_Exit;
+with Uart0;
 with PWM_API; use PWM_API;
 
 procedure Helios is
-   -- Create PWM
-   Pwm0 : PWM_T := Create(Channel => 0);
+   Pwm0 : PWM_T := Create (Channel => 0);
 begin
-   -- Configure PWM
-   Pwm0.Set_Hz(5.0);
-   Pwm0.Set_Duty_Cycle(0.5);
+   Uart0.Init (115200);
+   Pwm0.Configure (Target_Hz => 5.0, Duty => 0.5);
    Pwm0.Enable;
-
 end Helios;
-}
